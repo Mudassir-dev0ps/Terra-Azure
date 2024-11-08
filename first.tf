9521b98a04f8fb7c92f0f2d56b9602b2d6c14ee8
Mudassir-dev0ps <126445925+Mudassir-dev0ps@users.noreply.github.com>
2024-11-08 15:04:25 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,1 +1,18 @@
-abc
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2d23954b-b94b-4ae0-a74d-f18f6a6aee0b"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "latestrg"
+  location = "westus"
+}
